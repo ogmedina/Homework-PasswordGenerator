7d3202877d194e78a57e2a3606eb0aab4b4860a5
Oscar Medina <ogmedina04@gmail.com>
2020-10-29 10:05:17 -0500

    edited readme file with screenshots and links

--- Assets/script.js
+++ Assets/script.js
@@ -4,14 +4,13 @@ function generatePassword(){
 //Variables of different letters, numbers, and symbols
 var lowerCaseChar = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var upperCaseChar = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 var specialChar = ["!", '"', "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", '\\', "]", "^", "_", "`", "{", "|", "}", "~"];
 var pickedArray = [];
 
 var passwordLength = getPasswordLength();
 
 var charTypeSelected = false;
-
 
 function getPasswordLength() {
   var userChoice = 0;
@@ -22,18 +21,17 @@ function getPasswordLength() {
     if (isNaN(userChoice)) {
       userChoice = 0;      
     }
-    
   }
-
    return userChoice;
 }
 
 //loop for user to pick at least one character type
 while (charTypeSelected == false){
-  var lowerAlpha = getChoice("lowercase letters");
-  var upperAlpha = getChoice("uppercase letters");
+  var lowerAlpha = getChoice("lowercase");
+  var upperAlpha = getChoice("uppercase");
   var numChar = getChoice("numeric");
-  var specialPick = getChoice("special characters");
+  var specialPick = getChoice("special");
+  
   if ((lowerAlpha) || (upperAlpha) || (numChar) || (specialPick)){
     charTypeSelected = true;
   }
@@ -44,12 +42,12 @@ while (charTypeSelected == false){
 //PROBLEM HERE -- Cant cancel out of function
 //function for user choice options, instead of writing them all out
 function getChoice(currentOption){
-  var userChoice = "a";
+  var userChoice = "x";
    messagePrompt = "";
   var messagePrompt = ('Would you like '.concat(currentOption));
   messagePrompt = messagePrompt.concat(' characters (y/n)?');
   //loops ensures a valid response
-  while (userChoice = "a") {
+  while (userChoice = "x") {
     userChoice = (window.prompt(messagePrompt));
     //mobile users because sometimes the letters become capitalized
     userChoice = userChoice.toLowerCase();
@@ -62,9 +60,7 @@ function getChoice(currentOption){
       }
     }
   }
-
-
-
+  
 // this joins strings together
 if (lowerAlpha) {
   pickedArray = pickedArray.concat(lowerCaseChar);
@@ -88,16 +84,6 @@ for (var i= 0; i < passwordLength; i++) {
 return passwordString;
 
 }
-
-
-
-
-
-
-
-
-
-
 
 // references the generate element
  var generateBtn = document.querySelector("#generate");
--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# Homework-PasswordGenerator
-JavaScript Password Generator
+# Homework Javacript: Password Generator
+
+## Full Stack Book Camp Homework 3 - Password Generator
+
+### Description
+
+This is the third assignment for the UTSA Full-Stack Bootcamp. This is a password generator using JavaScript. The following items were the highlights of this assignment:
+
+* Button click generates a NEW password
+
+* Password Criteria Prompts
+
+* Prompt for length of password 8 to 128 characters
+
+* Selection of character types to include (lowercase, uppercase, numeric, and/or special characters)
+
+* Password generated that matches selected criteria
+
+* Password is written on page
+
+
+### Screenshots
+
+Here is a screenshot of the webpage for the password generator. 
+
+![javascript homework 1](./Images/javascript homework_n.jpg)
+
+![javascript homework 2](./Images/javascript homework_w.jpg)
+
+
+### Deployed Link
+
+Here is a link to the GitHub deployed site: [Password Generator](https://ogmedina.github.io/Homework-PasswordGenerator/)
