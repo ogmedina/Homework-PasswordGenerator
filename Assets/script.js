9cd0ff38c38f2d4b6a2b976514b9249026700ae6
Oscar Medina <ogmedina04@gmail.com>
2020-10-31 15:18:48 -0500

    edited numbers array

--- Assets/script.js
+++ Assets/script.js
@@ -4,7 +4,7 @@
 //Variables of different letters, numbers, and symbols
 var lowerCharArr = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var upperCharArr = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var numbers = ["0","1","2","3","4","5","6","7","8","9"];
+var numbers = [0,1,2,3,4,5,6,7,8,9];
 var specialChar = ["!", '"', "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", '\\', "]", "^", "_", "`", "{", "|", "}", "~"];
 
 //Event Listener for click of button
@@ -18,7 +18,7 @@ function writePassword() {
   if (passwordLength >= 8 && passwordLength <= 128) {    
   } 
   else {
-    return alert("Invalid response. Please enter a number between 8 and 128 and DO NOT use letters");
+    return alert("Invalid response. Please enter a number between 8 and 128 and DO NOT use letters");    
   }
 //Prompts user to confirm if they want a specific set of characters to use
   var lowerAlpha = confirm("Would you want your password to have lowercase letters? (abcdefg....) \n (OK = Yes) (Cancel = No)");
